f0e071e95a87cc45eb7334983aae6c266e74a6da
yzgit-0824 <yzgit-0824@users.noreply.github.com>
2025-02-15 01:21:28 +0000

    ✅ Ready to clone and code.

--- flask/ext/commands.py
+++ flask/ext/commands.py
@@ -1,8 +1,8 @@
 import click
 
-from project_name.ext.auth import create_user
-from project_name.ext.database import db
-from project_name.models import Product
+from flask.ext.auth import create_user
+from flask.ext.database import db
+from flask.models import Product
 
 
 def create_db():
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import sys
 import pytest
 
-from project_name import create_app
-from project_name.ext.commands import populate_db
-from project_name.ext.database import db
+from flask import create_app
+from flask.ext.commands import populate_db
+from flask.ext.database import db
 
 
 @pytest.fixture(scope="session")
